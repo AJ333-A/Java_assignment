1a8975a6eab750cee8b573f7e826c809c3536772
AJ333-A <joyashley2000@gmail.com>
2025-01-22 22:52:51 +0300

    Initial commit

---
+++ Addition.java
@@ -0,0 +1,20 @@
+//Program to add two numbers, in this case, Hardcoded values
+
+ //Author: Ashley Joy Akinyi
+ //Reg no: CT101/G/20035/23
+ //Date: 22/1/2025
+ //version 1.0
+
+//This is a public class called addition
+public class Addition {
+    //This is where the actual execution of the code happens
+    public static void main(String[] args) {
+        //initializing two integer x and y and assigning values to them
+        int x=5, y=7 ;
+        //storing the sum of the values of x and y in an integer called z
+        int z= x + y ;
+        //printing our the value of the addition of the two integers
+        System.out.println( "The sum is:"+ z);
+    }
+    
+}
---
+++ hello.java
@@ -0,0 +1,16 @@
+//This is a simple program to print out hello world  using Java
+
+ //Author: Ashley Joy Akinyi
+ //Reg no: CT101/G/20035/23
+ //Date: 22/1/2025
+ //version 1.0
+
+ //This is a public class that will contain the objects and the main part of the code
+public class hello {
+    //This is where the execution of the code begins
+    public static void main(String[] args) {
+        //This is the part that outputs hello world by using the java syntax
+        System.out.println("Hello world");
+    }
+    
+}
